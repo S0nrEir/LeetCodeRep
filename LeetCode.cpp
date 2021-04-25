d996ff0b4fe1738de881e78e83ac1eaa39d4a639
YangHaoChen <x73901@live.com>
2021-04-25 19:10:39 +0800

    .5最长回文子串

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,12 +1,13 @@
 ﻿#include <iostream>
 #include "MinimunPathSum.h"
 #include "PlusTwoNumber/PlusTwoNumber.h"
-
+#include "LongestPalindromicSubString/LongestPalindromicSubString.h"
 
 int main()
 {
 	//MinimunPathSum res;
-	PlusTwoNumber res;
+	//PlusTwoNumber res;
+	LongestPalindromicSubString res;
 	Entrance* entrance = &res;
 
 	entrance->Impl();
